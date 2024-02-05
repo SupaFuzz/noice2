71aee545cea2d89d4d124ededf43795817c0d567
Amy Hicox <amy@hicox.com>
2024-02-05 17:01:26 -0600

    noiceCoreUITable.js
    * pretty much done except editable cells and a direct CSV export (note 
    however, we already have the .data attribute which returns a good ol 2D 
    array, so maybe CSV integration oughta be external. Editable cells will 
    need noiceCoreUIFormElementInput and --- yeah that's kinda spicy. 
    nonetheless, this works real good and I'm thinking good enough initial 
    release. If we need more will hack on some more
    
    main.css
    * demo visual rules for coreUITable.js
    
    testUITwo.js
    * sets up test harness for coreUITable.js

--- demoApp/lib/UI/testUITwo.js
+++ demoApp/lib/UI/testUITwo.js
@@ -5,6 +5,7 @@
 import { noiceCoreUIScreen } from '../../../lib/noiceCoreUI.js';
 import { noiceObjectCore } from '../../../lib/noiceCore.js';
 import { noicePieChart } from '../../../lib/noicePieChart.js';
+import { noiceCoreUITable } from '../../../lib/noiceCoreUITable.js';
 
 class testUITwo extends noiceCoreUIScreen {
 
@@ -59,7 +60,42 @@ setupCallback(self){
 
     /*
         do thine setup here ye verily
+
+        LOH 2/2/24 @ 1723
+        next step: try to instantiate a noiceCoreUITable and see what breaks
+        it at least compiles so ... hey!
+
     */
+
+    // 2/2/24 @ 2030 -- I'm bored and I don't have anything better to do ...
+    try {
+        that.testTable = new noiceCoreUITable({
+            columns: [
+                { name: 'species', order: 1, type: 'char' },
+                { name: 'first', order: 2, type: 'char' },
+                { name: 'middle', order: 3, type: 'char', width: .5 },
+                { name: 'last', order: 4, type: 'char' },
+                { name: 'num', order: 5, type: 'int'}
+            ],
+            rows: [
+                { species: 'cat', first: 'Mo', middle: 'M', last: 'Hicox', num: 5 },
+                { species: 'cat', first: 'Jazzy', middle: 'J', last: 'Hicox', num: 82 },
+                { species: 'snail', first: 'Gary', middle: 'X', last: 'Squarepants', num: 1 },
+                { species: 'crustacean', first: 'Eugene', middle: "C", last: "Krabbs", num: 12 },
+                { species: 'canine', first: 'Scooby', middle: "D", last: "Doo", num: 420 },
+                { species: 'starfish', first: 'Patrick', middle: "", last: "Starr", num: 419 },
+            ],
+            // maxListHeight: '5em',
+            debug: false,
+            selectMode: 'single',
+            rowSelectCallback: async (selectBool, rowElement, selfRef) => {
+                console.log(`rowSelectCallback: ${selectBool} | ${rowElement.dataset.rownum}`);
+                return(true)
+            },
+            allowColumnSort: true
+        }).append(that._DOMElements.chartContainer);
+    }catch(e){ console.log(e); }
+
 }
 
 
