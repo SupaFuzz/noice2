1b3aeb51b72f79bc19cf73399e98b4c09333f872
Amy Hicox <amy@hicox.com>
2024-02-09 16:13:53 -0600

    test harness update

--- demoApp/lib/UI/testUITwo.js
+++ demoApp/lib/UI/testUITwo.js
@@ -69,56 +69,81 @@ setupCallback(self){
     */
 
     // 2/2/24 @ 2030 -- I'm bored and I don't have anything better to do ...
-    try {
-        that.testTable = new noiceCoreUITable({
-            firstArg: "hi there!",
-            columns: [
-                { name: 'species', order: 1, type: 'char' },
-                { name: 'first', order: 2, type: 'char' },
-                { name: 'middle', order: 3, type: 'char', width: .5 },
-                { name: 'last', order: 4, type: 'char' },
-                { name: 'num', order: 5, type: 'int'}
-            ],
-            rows: [
-                { species: 'cat', first: 'Mo', middle: 'M', last: 'Hicox', num: 5, test: "what" },
-                { species: 'cat', first: 'Jazzy', middle: 'J', last: 'Hicox', num: 82, test: "the" },
-                { species: 'snail', first: 'Gary', middle: 'X', last: 'Squarepants', num: 1, test: "heck" },
-                { species: 'crustacean', first: 'Eugene', middle: "C", last: "Krabbs", num: 12, test: "is" },
-                { species: 'canine', first: 'Scooby', middle: "D", last: "Doo", num: 420, test: "going"  },
-                { species: 'starfish', first: 'Patrick', middle: "", last: "Starr", num: 419, test: "on" },
-            ],
-            // maxListHeight: '5em',
-            debug: false,
-            selectMode: 'single',
-            rowSelectCallback: async (selectBool, rowElement, selfRef) => {
-                console.log(`rowSelectCallback: ${selectBool} | ${rowElement.dataset.rownum}`);
-                return(true)
-            },
-            allowColumnSort: true,
-            label: "test table",
-            /*
-            syncRowsBatchLimit: 1,
-            renderRowsProgressCallback: (partial, complete, selfReference) => {
-                console.log(`${partial}/${complete} (${Math.floor(partial/complete*100)})`);
-            }
-            */
-            allowCellEdit: true,
-            editCellCallback: async (rowElement, cellElement) => {
-                console.log(`inside editCellCallback with: ${rowElement.dataset.rownum} / ${cellElement.dataset.name}`);
-                return("this is test");
-            },
-            modifyRowCallback:  async(row, data) => {
-                console.log(`inside modifyRowCallback`);
-                console.log(row);
-                console.log(data);
-                return(data);
-            }
-        }).append(that._DOMElements.chartContainer);
-    }catch(e){ console.log(e); }
+    that.testTable = new noiceCoreUITable({
+        columns: [
+            { name: 'species', order: 1, type: 'char' },
+            { name: 'first', order: 2, type: 'char' },
+            { name: 'middle', order: 3, type: 'char', width: '5em' },
+            { name: 'last', order: 4, type: 'char' },
+            { name: 'num', order: 5, type: 'int'}
+        ],
+        rows: [
+            { species: 'cat', first: 'Mo', middle: 'M', last: 'Hicox', num: 5, test: "what" },
+            { species: 'cat', first: 'Jazzy', middle: 'J', last: 'Hicox', num: 82, test: "the" },
+            { species: 'snail', first: 'Gary', middle: 'X', last: 'Squarepants', num: 1, test: "heck" },
+            { species: 'crustacean', first: 'Eugene', middle: "C", last: "Krabbs", num: 12, test: "is" },
+            { species: 'canine', first: 'Scooby', middle: "D", last: "Doo", num: 420, test: "going"  },
+            { species: 'starfish', first: 'Patrick', middle: "", last: "Starr", num: 419, test: "on" },
+        ],
+        // maxListHeight: '5em',
+        debug: false,
+        selectMode: 'single',
+        rowSelectCallback: async (selectBool, rowElement, selfRef) => {
+            console.log(`rowSelectCallback: ${selectBool} | ${rowElement.dataset.rownum}`);
+            return(true)
+        },
+        allowColumnSort: true,
+        label: "test table",
+        /*
+        syncRowsBatchLimit: 1,
+        renderRowsProgressCallback: (partial, complete, selfReference) => {
+            console.log(`${partial}/${complete} (${Math.floor(partial/complete*100)})`);
+        }
+        */
+        allowCellEdit: true,
+        //editCellCallback: async (rowElement, cellElement, tableRef) => { return(that.handleCellEdit(rowElement, cellElement, tableRef)); },
+        modifyRowCallback:  async(row, data) => {
+            console.log(`inside modifyRowCallback`);
+            console.log(row);
+            console.log(data);
+            return(data);
+        }
+    }).append(that._DOMElements.chartContainer);
 
 }
 
 
+
+
+/*
+    handleCellEdit(rowElement, celElement, tableRef)
+*/
+handleCellEdit(rowElement, cellElement, tableRef){
+    let that = this;
+    return(new Promise((toot, boot) => {
+
+        let c = cellElement.getBoundingClientRect();
+
+
+        // make an editor I guess, or something like that
+        let inp = document.createElement('input');
+        inp.setAttribute('type', 'text');
+        inp.value = cellElement.textContent;
+        inp.style.width = `${(c.width - 4)}px`;
+        inp.addEventListener('focusout', (evt) => {
+            let v = inp.value;
+            inp.remove();
+            toot(v);
+        });
+        inp.addEventListener('keydown', (evt) => {
+            if (evt.keyCode == 13){ inp.blur(); }
+        });
+
+        cellElement.innerHTML = '';
+        cellElement.appendChild(inp);
+
+    }));
+}
 
 
 /*
