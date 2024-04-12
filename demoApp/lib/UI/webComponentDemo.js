1bd79bb9b22399d44cc7e2b2efc7dcffa3d6e8eb
Amy Hicox <amy@hicox.com>
2024-04-12 10:37:38 -0500

    webComponentDemo.js * some test buttons n stuff

--- demoApp/lib/UI/webComponentDemo.js
+++ demoApp/lib/UI/webComponentDemo.js
@@ -35,7 +35,14 @@ constructor(args, defaults, callback){
 */
 get html(){return(`
     <h1>Web Component Demo</h1>
-    <wp-pie-chart id="testMe" size="20em" value="20" badge_text="donks" show_badge="true" badge_position="bottom" multiple_chart_mode="stack" />
+
+    <div class="pieChartDemo" style="display:grid;place-items:center;">
+        <wp-pie-chart id="testMe" size="20em" badge_text="test" show_badge="true" badge_position="bottom"></wp-pie-chart>
+        &nbsp;
+        &nbsp;
+        <button id="btnAddChart">add charts</button>
+        <button id="btnToggleMode">layout mode</button>
+    </div>
 `)}
 
 
@@ -47,9 +54,21 @@ get html(){return(`
 */
 setupCallback(self){
     let that = this;
-    that.DOMElement.style = "grid";
+    that.DOMElement.style.display = "grid";
 
-    that.testThing = that.DOMElement.querySelector('#testMe');
+    that.testPie = that.DOMElement.querySelector('#testMe');
+
+    that.DOMElement.querySelector('#btnAddChart').addEventListener('click', (evt) =>{
+        // I ain't fibbin' ... or am i?
+        [2, 3 ,34, 8, 13, 5, 1, 21 ].forEach((fn) => {
+            that.testPie.addChart({name: `L${fn}`, value: fn, chart_color: `rgba(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)}, .8)` });
+        });
+    });
+
+    that.DOMElement.querySelector('#btnToggleMode').addEventListener('click', (evt) =>{
+        that.testPie.multiple_chart_mode = (that.testPie.multiple_chart_mode == "stack")?'overlay':'stack';
+        that.testPie.badge_text = `chart layout: ${that.testPie.multiple_chart_mode}`;
+    });
 
 }
 
