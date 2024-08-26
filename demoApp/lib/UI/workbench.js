c50f1849c785405ad5f15dff5cc69f767f2fb95c
Amy Hicox <amy@hicox.com>
2024-08-26 17:16:01 -0500

    wcBaloonDialog.js * implements headerContent as optional slot
    wcToggle.js
    * click handlers now evt.stopPropagation()
    
    wcCollapsibleObject.js
    new, implements the old "spring loaded section" jquery thing but as a 
    webComponent with slots
    
    workbench.js
    * testHarness for wcCollasbsibleObject

--- demoApp/lib/UI/workbench.js
+++ demoApp/lib/UI/workbench.js
@@ -5,7 +5,10 @@
 import { noiceCoreUIScreen } from '../../../lib/noiceCoreUI.js';
 import { noiceObjectCore } from '../../../lib/noiceCore.js';
 import { wcLeftPanelLayout } from '../../../lib/webComponents/wcLeftPanelLayout.js';
-import { wcSelectableObject } from '../../../lib/webComponents/wcSelectableObject.js'
+import { wcSelectableObject } from '../../../lib/webComponents/wcSelectableObject.js';
+import { wcCollapsibleObject } from '../../../lib/webComponents/wcCollapsibleObject.js';
+import { wcToggle } from '../../../lib/webComponents/wcToggle.js';
+import { wcBasic } from '../../../lib/webComponents/wcBasic.js';
 
 class workbenchUI extends noiceCoreUIScreen {
 
@@ -36,13 +39,49 @@ constructor(args, defaults, callback){
 get html(){
     return(`
         <wc-left-panel-layout title="test panel">
+            <div slot="main-content">
 
-            <!-- lets make a test menu -->
-            <div slot="main-content" style="display: grid; padding: 1em; user-select: none;" data-_name="testMenu">
-                <wc-selectable-object data-_name="one"><span slot="content">one</span></wc-selectable-object>
-                <wc-selectable-object data-_name="two"><span slot="content">two</span></wc-selectable-object>
-                <wc-selectable-object data-_name="three"><span slot="content">three</span></wc-selectable-object>
-                <wc-selectable-object data-_name="four"><span slot="content">four</span></wc-selectable-object>
+                <!-- lets make a test menu -->
+                <div data-_name="testMenu" style="display: grid; padding: 1em; user-select: none;">
+                    <wc-selectable-object data-_name="one"><span slot="content">one</span></wc-selectable-object>
+                    <wc-selectable-object data-_name="two"><span slot="content">two</span></wc-selectable-object>
+                    <wc-selectable-object data-_name="three"><span slot="content">three</span></wc-selectable-object>
+                    <wc-selectable-object data-_name="four"><span slot="content">four</span></wc-selectable-object>
+                </div>
+
+
+                <!-- test a collapsible object -->
+                <wc-collapsible-object open_height="30em" handle_indicator="true" click_to_collapse="true">
+                    <!--<span slot="handle">click me</span>-->
+                    <div slot="content" style="display: grid;">
+
+                        <wc-toggle label="show only active"></wc-toggle>
+                        <wc-toggle label="show only at my location" on="true"></wc-toggle>
+                        <wc-toggle label="show only completed"></wc-toggle>
+
+                        <!--
+                        <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Ad malesuada aenean est et dignissim iaculis ultrices. Iaculis platea erat velit felis sapien. Orci ante quis ornare odio justo. Dictum lobortis ultricies commodo scelerisque pulvinar odio nascetur. Fames sagittis nec imperdiet posuere euismod lorem penatibus lobortis. Mauris nascetur magnis amet purus dui aenean suspendisse fames purus.</p>
+
+                        <p>Dis aliquam inceptos fusce himenaeos proin dui mauris cras libero. Praesent taciti vel imperdiet sodales sem porttitor integer faucibus habitasse. Nulla eleifend enim amet curabitur nam vitae. Egestas sed ullamcorper senectus tempor dapibus leo. Lacinia maximus vestibulum in massa lectus purus ullamcorper efficitur. Aliquet interdum sociosqu nisi posuere interdum nostra urna vehicula. Vestibulum amet elementum, odio montes varius nascetur nascetur vestibulum. Sit imperdiet varius ad cubilia auctor. Torquent primis magna pellentesque ridiculus, himenaeos himenaeos varius.</p>
+
+                        <p>Cubilia efficitur quis ornare facilisi malesuada facilisis nec quam aenean. Et blandit ligula venenatis placerat nostra justo molestie. Pharetra potenti aliquet pharetra eu accumsan platea velit. Tincidunt dui risus proin velit consectetur velit nam phasellus cursus. Blandit donec tortor porttitor viverra velit nisi tristique. Nisl mattis praesent interdum nisl vivamus. Sit parturient tempor ornare aenean tortor fames est nulla class. Urna erat libero habitant odio faucibus placerat. Metus praesent erat varius tincidunt varius.</p>
+
+                        <p>Rhoncus at nisl mauris nascetur nisi porta mi. Enim etiam vehicula per amet penatibus nam orci elit. Duis odio praesent libero ridiculus vivamus; taciti luctus lacinia. Curae volutpat luctus velit turpis urna dictum ex pulvinar massa. Consequat odio cursus morbi; penatibus natoque montes luctus. Aptent eget tempor id lorem senectus at quis. Maecenas ipsum tortor sodales fusce cubilia aenean proin molestie. Efficitur sodales luctus nisi non habitasse pharetra justo nostra.</p>
+
+                        <p>Rutrum tempus pretium vulputate urna ad a. Nisl est mattis proin ad cubilia sodales rhoncus lorem lacus. Penatibus porta parturient sem habitant praesent ad euismod interdum. Porta dolor augue aliquam varius hac nascetur aptent ante. Non sollicitudin tempus duis diam non; torquent aliquam. Morbi et felis duis suspendisse penatibus quam vehicula fusce. Quam euismod euismod ut, venenatis quisque cursus. Tellus ad vel in praesent mollis quisque tristique sed.</p>
+
+                        <p>Dis aliquam inceptos fusce himenaeos proin dui mauris cras libero. Praesent taciti vel imperdiet sodales sem porttitor integer faucibus habitasse. Nulla eleifend enim amet curabitur nam vitae. Egestas sed ullamcorper senectus tempor dapibus leo. Lacinia maximus vestibulum in massa lectus purus ullamcorper efficitur. Aliquet interdum sociosqu nisi posuere interdum nostra urna vehicula. Vestibulum amet elementum, odio montes varius nascetur nascetur vestibulum. Sit imperdiet varius ad cubilia auctor. Torquent primis magna pellentesque ridiculus, himenaeos himenaeos varius.</p>
+
+                        <p>Cubilia efficitur quis ornare facilisi malesuada facilisis nec quam aenean. Et blandit ligula venenatis placerat nostra justo molestie. Pharetra potenti aliquet pharetra eu accumsan platea velit. Tincidunt dui risus proin velit consectetur velit nam phasellus cursus. Blandit donec tortor porttitor viverra velit nisi tristique. Nisl mattis praesent interdum nisl vivamus. Sit parturient tempor ornare aenean tortor fames est nulla class. Urna erat libero habitant odio faucibus placerat. Metus praesent erat varius tincidunt varius.</p>
+
+                        <p>Rhoncus at nisl mauris nascetur nisi porta mi. Enim etiam vehicula per amet penatibus nam orci elit. Duis odio praesent libero ridiculus vivamus; taciti luctus lacinia. Curae volutpat luctus velit turpis urna dictum ex pulvinar massa. Consequat odio cursus morbi; penatibus natoque montes luctus. Aptent eget tempor id lorem senectus at quis. Maecenas ipsum tortor sodales fusce cubilia aenean proin molestie. Efficitur sodales luctus nisi non habitasse pharetra justo nostra.</p>
+                        -->
+
+                    </div>
+                </wc-collapsible-object>
+
+                <div data-_name="testFrame"></div>
+
             </div>
         </wc-left-panel-layout>
     `);
@@ -75,7 +114,52 @@ setupCallback(self){
                 });
             }
         }
-    })
+    });
+
+    let thing = new wcBasic({
+        content: `
+            <div data-_name="handle">open</div>
+            <div class="content" data-_name="content" data-open="false">
+                <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Ad malesuada aenean est et dignissim iaculis ultrices. Iaculis platea erat velit felis sapien. Orci ante quis ornare odio justo. Dictum lobortis ultricies commodo scelerisque pulvinar odio nascetur. Fames sagittis nec imperdiet posuere euismod lorem penatibus lobortis. Mauris nascetur magnis amet purus dui aenean suspendisse fames purus.</p>
+
+                <p>Dis aliquam inceptos fusce himenaeos proin dui mauris cras libero. Praesent taciti vel imperdiet sodales sem porttitor integer faucibus habitasse. Nulla eleifend enim amet curabitur nam vitae. Egestas sed ullamcorper senectus tempor dapibus leo. Lacinia maximus vestibulum in massa lectus purus ullamcorper efficitur. Aliquet interdum sociosqu nisi posuere interdum nostra urna vehicula. Vestibulum amet elementum, odio montes varius nascetur nascetur vestibulum. Sit imperdiet varius ad cubilia auctor. Torquent primis magna pellentesque ridiculus, himenaeos himenaeos varius.</p>
+
+                <p>Cubilia efficitur quis ornare facilisi malesuada facilisis nec quam aenean. Et blandit ligula venenatis placerat nostra justo molestie. Pharetra potenti aliquet pharetra eu accumsan platea velit. Tincidunt dui risus proin velit consectetur velit nam phasellus cursus. Blandit donec tortor porttitor viverra velit nisi tristique. Nisl mattis praesent interdum nisl vivamus. Sit parturient tempor ornare aenean tortor fames est nulla class. Urna erat libero habitant odio faucibus placerat. Metus praesent erat varius tincidunt varius.</p>
+
+                <p>Rhoncus at nisl mauris nascetur nisi porta mi. Enim etiam vehicula per amet penatibus nam orci elit. Duis odio praesent libero ridiculus vivamus; taciti luctus lacinia. Curae volutpat luctus velit turpis urna dictum ex pulvinar massa. Consequat odio cursus morbi; penatibus natoque montes luctus. Aptent eget tempor id lorem senectus at quis. Maecenas ipsum tortor sodales fusce cubilia aenean proin molestie. Efficitur sodales luctus nisi non habitasse pharetra justo nostra.</p>
+
+                <p>Rutrum tempus pretium vulputate urna ad a. Nisl est mattis proin ad cubilia sodales rhoncus lorem lacus. Penatibus porta parturient sem habitant praesent ad euismod interdum. Porta dolor augue aliquam varius hac nascetur aptent ante. Non sollicitudin tempus duis diam non; torquent aliquam. Morbi et felis duis suspendisse penatibus quam vehicula fusce. Quam euismod euismod ut, venenatis quisque cursus. Tellus ad vel in praesent mollis quisque tristique sed.</p>
+
+                <p>Dis aliquam inceptos fusce himenaeos proin dui mauris cras libero. Praesent taciti vel imperdiet sodales sem porttitor integer faucibus habitasse. Nulla eleifend enim amet curabitur nam vitae. Egestas sed ullamcorper senectus tempor dapibus leo. Lacinia maximus vestibulum in massa lectus purus ullamcorper efficitur. Aliquet interdum sociosqu nisi posuere interdum nostra urna vehicula. Vestibulum amet elementum, odio montes varius nascetur nascetur vestibulum. Sit imperdiet varius ad cubilia auctor. Torquent primis magna pellentesque ridiculus, himenaeos himenaeos varius.</p>
+
+                <p>Cubilia efficitur quis ornare facilisi malesuada facilisis nec quam aenean. Et blandit ligula venenatis placerat nostra justo molestie. Pharetra potenti aliquet pharetra eu accumsan platea velit. Tincidunt dui risus proin velit consectetur velit nam phasellus cursus. Blandit donec tortor porttitor viverra velit nisi tristique. Nisl mattis praesent interdum nisl vivamus. Sit parturient tempor ornare aenean tortor fames est nulla class. Urna erat libero habitant odio faucibus placerat. Metus praesent erat varius tincidunt varius.</p>
+
+                <p>Rhoncus at nisl mauris nascetur nisi porta mi. Enim etiam vehicula per amet penatibus nam orci elit. Duis odio praesent libero ridiculus vivamus; taciti luctus lacinia. Curae volutpat luctus velit turpis urna dictum ex pulvinar massa. Consequat odio cursus morbi; penatibus natoque montes luctus. Aptent eget tempor id lorem senectus at quis. Maecenas ipsum tortor sodales fusce cubilia aenean proin molestie. Efficitur sodales luctus nisi non habitasse pharetra justo nostra.</p>
+            </div>
+        `,
+        initializedCallback: (slf) => {
+            slf._elements.handle.addEventListener('click', (evt) => {
+                slf._elements.content.dataset.open = (!(slf._elements.content.dataset.open == "true"));
+                slf._elements.handle.textContent = (slf._elements.content.dataset.open == "true")?'close':'open';
+            });
+        },
+        styleSheet: `
+            div.content {
+                display: block;
+                overflow: auto;
+                transition: height .5s;
+            }
+            div.content[data-open="false"] {
+                height: 0px;
+            }
+            div.content[data-open="true"] {
+                height: 40em;
+            }
+        `
+    });
+    // that.DOMElement.querySelector('div[data-_name="testFrame"]').appendChild(thing);
+
+
 
 
     /* brute force add button
