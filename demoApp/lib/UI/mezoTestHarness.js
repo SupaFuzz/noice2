13d17ce6345af8958e31d7b08861f957fb8eea41
Amy Hicox <amy@hicox.com>
2025-01-14 19:17:00 -0600

    mezoTestHarness.js works through login, list and download needs upload next

--- demoApp/lib/UI/mezoTestHarness.js
+++ demoApp/lib/UI/mezoTestHarness.js
@@ -6,6 +6,7 @@ import { noiceCoreUIScreen } from '../../../lib/noiceCoreUI.js';
 import { noiceObjectCore } from '../../../lib/noiceCore.js';
 import { wcFormElement } from '../../../lib/webComponents/wcFormElement.js';
 import { wcBalloonDialog } from '../../../lib/webComponents/wcBalloonDialog.js';
+import { wcPieChart } from '../../../lib/webComponents/wcPieChart.js';
 import { wcTable } from '../../../lib/webComponents/wcTable.js';
 import { noiceMezoAPI } from '../../../lib/noiceMezoAPI.js';
 
@@ -45,7 +46,7 @@ get html(){return(`
 
         <div style="display: grid; grid-template-columns: auto auto;">
             <div class="formElements">
-                <wc-form-element type="text" data-templatename="proxypath" data-templateattribute="true" label="proxypath" default_value="/REST"></wc-form-element>
+                <wc-form-element type="text" data-templatename="proxypath" data-templateattribute="true" label="proxypath" default_value="/REST" capture_value_on="focusoutOrReturn"></wc-form-element>
                 <wc-form-element type="text" data-templatename="username" data-templateattribute="true" label="user"></wc-form-element>
                 <wc-form-element type="password" data-templatename="pass" data-templateattribute="true" label="password"></wc-form-element>
                 <button id="btnAuth" data-templatename="btnAuth" data-templateattribute="true" style="width:max-content; margin-top:.5em;">Log In</button>
@@ -80,10 +81,10 @@ setupCallback(self){
     let fileList = new wcTable({
         label: 'mezo.files',
         columns: [
-            { name: 'id', order: 1, type: 'int', width: '5em', disableCellEdit: true },
-            { name: 'type', order: 2, type: 'char', width: '8em', disableCellEdit: true },
-            { name: 'name', order: 3, type: 'char', width: '15em' },
-            { name: 'size', order: 4, type: 'int', width: '8em', disableCellEdit: true }
+            { name: 'id', order: 1, type: 'int', width: '5em' },
+            { name: 'type', order: 2, type: 'char', width: '8em'},
+            { name: 'name', order: 3, type: 'char', width: '15em', disableCellEdit: false },
+            { name: 'size', order: 4, type: 'int', width: '8em'}
         ],
         rows: [],
         select_mode: 'single',
@@ -94,8 +95,9 @@ setupCallback(self){
         show_btn_select_none: true,
         show_btn_export: true,
         show_btn_search: true,
-        allow_cell_edit: true,
+        allow_cell_edit: false,
         fit_parent: true,
+        rowDblClickCallback: async (rowElement, span, tblRef) => { return(that.handleRowDblClick(rowElement, span, tblRef)); }
     });
     that._DOMElements.tableContainer.appendChild(fileList);
 
@@ -108,7 +110,7 @@ setupCallback(self){
     });
 
     // bind proxyPath value change to attribute
-    that._DOMElements.proxypath.valueChangeCallback = (n,o,s) => {
+    that._DOMElements.proxypath.captureValueCallback = (n,s) => {
         console.log(`set new proxyPath: ${n}`);
         that.api.proxyPath = n;
     };
@@ -150,6 +152,83 @@ setupCallback(self){
             and initially rendering the table content
         */
     });
+}
+
+
+
+
+/*
+    handleRowDblClick(rowElement, span, tblRef)
+    pop-a-dilly, show download progress, make a file
+    download link or if it's an image, show it
+*/
+handleRowDblClick(rowElement, span, tblRef){
+    let that = this;
+    return(new Promise((toot, boot) => {
+        let dta = JSON.parse(rowElement.dataset.rowdata);
+
+        // hey, what's ...
+        let theDillyYo = new wcBalloonDialog({
+            arrow_position: 'none',
+            lightbox: true,
+            title: dta.name,
+            exitCallback: async (dilly) => {
+                toot(true);
+            }
+        });
+
+        // the content
+        let div = document.createElement('div');
+        div.style.display = "grid";
+        div.style.gridTemplateColumns = "auto auto";
+        div.style.placeContent = "center";
+        div.setAttribute('slot', "dialogContent");
+        div.innerHTML = `
+            <wc-pie-chart size="5.5em"></wc-pie-chart>
+            <h2>loading ...</h2>
+        `;
+        theDillyYo.appendChild(div);
+        theDillyYo.relativeElement = that.DOMElement;
+        that.DOMElement.appendChild(theDillyYo);
+
+        // get get um!
+        this.api.getFile({
+            fileName: dta.name,
+            progressCallback: (r, t, d) => {
+                div.querySelector('wc-pie-chart').value = ((r/t)*100);
+            }
+        }).then((file) => {
+            const blob = new Blob([file.data], { type: file.type });
+
+            // if it's an image, just display it as the css backgroundImage
+            if (/^image\//i.test(file.type)){
+                let lnk = URL.createObjectURL(blob);
+                div.style.background = `url('${lnk}')`;
+                div.style.backgroundSize = "contain";
+                div.style.backgroundRepeat = "no-repeat";
+                div.style.backgroundPosition = "center";
+            }else{
+                // just make a link that looks like a button to download it
+                const fileURL = URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.href = fileURL;
+                a.download = file.file_name;
+                a.textContent = `download ${file.file_name}`;
+                div.appendChild(a);
+            }
+
+            // either way remove the progress indicator and message
+            let v = div.getBoundingClientRect();
+            div.style.width = `${v.width}px`;
+            div.style.height = `${v.height}px`;
+            div.querySelector('wc-pie-chart ').remove();
+            div.querySelector('h2').remove();
+
+        }).catch((error) => {
+            console.log(`getFile() | ${error}`);
+        })
+
+    }));
 }
 
 
