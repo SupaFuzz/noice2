ef461fff5ea670ba2d86c69dab43fd862caa7f98
Amy Hicox <amy@hicox.com>
2025-05-03 00:17:29 -0500

    wcMainUI.js * new -- wip, mostly works
    wcWorkBench/*
    * a more direct way of messing with web components than the full demo

---
+++ wcWorkbanch/lib/main.js
@@ -0,0 +1,10 @@
+import { wcPieChart } from '../../lib/webComponents/wcPieChart.js';
+import { wcMainUI } from '../../lib/webComponents/wcMainUI.js';
+
+
+/*
+    document.loaded() hook
+*/
+document.addEventListener("DOMContentLoaded", (evt) => {
+    // could do some stuff here if ya wanted
+});
