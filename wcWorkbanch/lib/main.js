8f971ede9d78a1abbe232e6f5e8204b40540483d
Amy Hicox <amy@hicox.com>
2025-05-16 23:27:03 -0500

    wcScreenHolder.js * imports wcBasic * getUIMenu() - implemented, returns menu with hooks to drive switchUI() -- returned elemet live updates from addUI() and returns a wcBasic * tons of cssVars to implement menu appearance * implements a mutationObserver to detect child node tree changes, this detects new slots being added from the DOM, and initiates them lifting them into the shadowDOM (and sending the ui_added signal which updates the menu) * wcWorkBanch stuff updated to demonstrate new stuffs above

--- wcWorkbanch/lib/main.js
+++ wcWorkbanch/lib/main.js
@@ -14,53 +14,9 @@ document.addEventListener("DOMContentLoaded", (evt) => {
     window.uiHolder = document.body.querySelector('wc-screen-holder');
     window.mainUI = document.body.querySelector('wc-main-ui');
 
-    // make a burgerMenu of the UIs on the screenHolder
-    window.burgerMenu = new wcBasic({
-        content: Object.keys(window.uiHolder.UIs).map((a) => {return(window.uiHolder.UIs[a])}).sort((a,b) => {return(
-                    parseFloat(a.dataset.menu_order) - parseFloat(b.dataset.menu_order)
-                 )}).map((el) => {return(`<button class="menuBtn" data-_name="${el.dataset.name}">${el.dataset.menu_label}</button>`)}).join(""),
-        styleSheet: `
-            :host {
-                display: grid;
-                overflow: auto;
-            }
-            button {
-                color: var(--wc-main-ui-header-color);
-                background-color: transparent;
-                border-color: transparent;
-                width: 100%;
-                text-align: right;
-                font-size: 1.5rem;
-                font-family: Comfortaa;
-                width: 10em;
-            }
-            button:hover {
-                color: var(--wc-main-ui-user-prompt-alert-color);
-                background-color: var(--wc-main-ui-header-color);
-            }
-            button[data-selected="true"]{
-                background-color: rgba(240, 240, 240, .2);
-            }
-        `,
-        initializedCallback: (slf) => {
-            slf.shadowDOM.querySelectorAll('button').forEach((el) => {
-                el.addEventListener("click", (evt) => {
-                    window.uiHolder.switchUI(el.dataset._name).then(() => {
-                        Object.keys(slf._elements).map((a) => {return(slf._elements[a])}).filter((ell) => {return(
-                            ell.className == "menuBtn" &&
-                            (ell.dataset._name != el.dataset._name)
-                        )}).forEach((ell) => {ell.dataset.selected = "false"; })
-                        el.dataset.selected = "true";
-                    });
-                });
-            })
-        }
-    });
+    // send the default menu from the uiHolder to the burgerMenu in the balloonDialog
     window.mainUI.burgerMenuContent.innerHTML = '';
-    window.mainUI.burgerMenuContent.appendChild(window.burgerMenu);
-
-
-    //window.mainUI.burgerMenuContent.textContent = "it worked?";
+    window.mainUI.burgerMenuContent.appendChild(window.uiHolder.getUIMenu());
 
     // put an are you sure? on the test2 screen
     window.uiHolder.UIs.test2.setFocus = (focusBool, focusArgs) => { return(new Promise((toot, boot) => {
@@ -84,6 +40,5 @@ document.addEventListener("DOMContentLoaded", (evt) => {
             toot(window.uiHolder.UIs.test2);
         }
     }))}
-
-
+    
 });
